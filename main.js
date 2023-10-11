3c7334660e721f7e0502cf7a9fb3f340f1e3cd75
profbt <bruno.barros.carvalho@escola.pr.gov.br>
2023-10-11 15:30:02 -0300

    ok

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 function tocaSomGrilo () {
     document.querySelector("#som_tecla_grilo").play();
-    document.querySelector("#som_tecla_grilo").play();
+    
+}
+
+function tocaSomAplasusos () {
     document.querySelector("#som_tecla_aplausos").play();
+    
 }
 
 document.querySelector(".tecla_grilo").onclick = tocaSomGrilo;
-document.querySelector(".tecla_aplausos").onclick = tocaSomAplausos;
+document.querySelector(".tecla_aplausos").onclick = tocaSomAplasusos;
+
 
 
