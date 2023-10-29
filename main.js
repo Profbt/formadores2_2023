e0f77a67d794910aa3ec561025304e758a0b12ed
Professor Bruno Carvalho <101364708+Profbt@users.noreply.github.com>
2023-10-29 23:26:21 +0000

    if-evento

--- main.js
+++ main.js
@@ -5,22 +5,25 @@ function tocaSom (idAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while(contador < listaDeTeclas.length) {
+for(let contador = 0;contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`;
     
     tecla.onclick = function () {
         tocaSom (idAudio);
     }
-    contador = contador + 1;
+    
+    tecla.onkeydown = function (evento) {
 
-    console.log(contador);
+        if (evento.code === 'Space' || evento.code === 'Enter') 
+        tecla.classList.add('ativa');
+    }
 
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
 
 
