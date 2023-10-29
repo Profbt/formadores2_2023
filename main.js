f3d85c870340f970d9f3127aeb46801d16653bdd
Professor Bruno Carvalho <101364708+Profbt@users.noreply.github.com>
2023-10-29 17:36:16 +0000

    ok

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
-function tocaSomRisada () {
-    document.querySelector("#som_tecla_risada").play();
-    
-}
-
-const listaDeTeclas = document.querySelectorAll(".tecla");
-listaDeTeclas[2].onclick = tocaSomRisada;
-
 function tocaSomAplausos () {
-    document.querySelector("#som_tecla_aplausos").play();
-    
+    document.querySelector('#som_tecla_aplausos').play();
 }
 
-const listaDeTeclas = document.querySelectorAll(".tecla");
-listaDeTeclas[0].onclick = tocaSomAplausos;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
+
+while() {
+    listaDeTeclas[0].onclick = tocaSomAplausos;
+}
 
 
